aa68a7490dded45c99d1dac63a2a42d73f75ce4e
Skyler Richter <skylerrichter@gmail.com>
2025-01-04 20:32:15 -0800

    generate random number

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,45 @@
-use std::io::stdin;
+use std::{
+    io::stdin,
+    time::{SystemTime, UNIX_EPOCH},
+};
+
+fn seconds_since_epoch() -> u64 {
+    SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_secs()
+}
+
+fn rand() -> u32 {
+    (seconds_since_epoch() as u32 % 8) + 1
+}
 
 fn main() {
-    let number = "5";
+    let number = rand().to_string();
 
-    println!("Guess the number: ");
+    println!("Guess the number between 1 and 9 (or type `quit` to exit):");
 
     loop {
         let mut input = String::new();
 
-        stdin().read_line(&mut input).unwrap();
+        if let Err(_) = stdin().read_line(&mut input) {
+            println!("Failed to read input. Please try again.");
+            continue;
+        };
 
-        match input.trim() {
-            "quit" => return,
-            guess if guess == number => return,
+        let guess = input.trim();
+
+        match guess {
+            "quit" => {
+                println!("Goodbye!");
+                return;
+            }
+            _ if guess == number => {
+                println!("You guessed it right! The number was: {}", number);
+                return;
+            }
             _ => {
-                println!("Guess again!");
+                println!("Incorrect guess. Try again!");
             }
         }
     }
