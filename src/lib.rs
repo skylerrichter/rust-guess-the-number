005b4073cf9d439f4d795d018286f873d30fda82
Skyler Richter <skylerrichter@gmail.com>
2025-01-09 21:50:14 -0800

    move game to lib

---
+++ src/lib.rs
@@ -0,0 +1,61 @@
+use std::{
+    io::stdin,
+    time::{SystemTime, SystemTimeError, UNIX_EPOCH},
+};
+
+fn seconds_since_epoch() -> Result<u64, SystemTimeError> {
+    SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .map(|duration| duration.as_secs())
+}
+
+fn rand() -> u32 {
+    match seconds_since_epoch() {
+        Ok(seconds) => (seconds as u32 % 8) + 1,
+        Err(_) => 1,
+    }
+}
+
+#[derive(PartialEq)]
+pub enum State {
+    Quit,
+    Win,
+    Loose,
+}
+
+pub struct Game {
+    pub state: Option<State>,
+    number: u32,
+}
+
+impl Game {
+    pub fn new() -> Self {
+        Self {
+            state: None,
+            number: rand(),
+        }
+    }
+
+    pub fn update(&mut self) {
+        let mut input = String::new();
+
+        if let Err(_) = stdin().read_line(&mut input) {
+            println!("Failed to read input. Please try again.");
+        };
+
+        match input.trim() {
+            "quit" => self.state = Some(State::Quit),
+            text if text == self.number.to_string() => self.state = Some(State::Win),
+            _ => self.state = Some(State::Loose),
+        }
+    }
+
+    pub fn draw(&mut self) {
+        match self.state {
+            Some(State::Quit) => println!("Quit!"),
+            Some(State::Win) => println!("Win!"),
+            Some(State::Loose) => println!("Loose!"),
+            _ => println!("Whoops!"),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,6 @@
-use std::{
-    fmt::Error,
-    io::stdin,
-    time::{SystemTime, SystemTimeError, UNIX_EPOCH},
-};
+use std::fmt::Error;
 
-fn seconds_since_epoch() -> Result<u64, SystemTimeError> {
-    SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .map(|duration| duration.as_secs())
-}
-
-fn rand() -> u32 {
-    match seconds_since_epoch() {
-        Ok(seconds) => (seconds as u32 % 8) + 1,
-        Err(_) => 1,
-    }
-}
-
-#[derive(PartialEq)]
-enum State {
-    Quit,
-    Win,
-    Loose,
-}
-
-struct Game {
-    state: Option<State>,
-    number: u32,
-}
-
-impl Game {
-    fn new() -> Self {
-        Self {
-            state: None,
-            number: rand(),
-        }
-    }
-
-    fn update(&mut self) {
-        let mut input = String::new();
-
-        if let Err(_) = stdin().read_line(&mut input) {
-            println!("Failed to read input. Please try again.");
-        };
-
-        match input.trim() {
-            "quit" => self.state = Some(State::Quit),
-            text if text == self.number.to_string() => self.state = Some(State::Win),
-            _ => self.state = Some(State::Loose),
-        }
-    }
-
-    fn draw(&mut self) {
-        match self.state {
-            Some(State::Quit) => println!("Quit!"),
-            Some(State::Win) => println!("Win!"),
-            Some(State::Loose) => println!("Loose!"),
-            _ => println!("Whoops!"),
-        }
-    }
-}
+use guess_the_number::{Game, State};
 
 fn main() -> Result<(), Error> {
     let mut game = Game::new();
